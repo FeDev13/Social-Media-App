095c8f5b44c50a60556c4e74f19cd2f19b46a3d6
Juanpy <109622347+JuanpyMoscoloni@users.noreply.github.com>
2023-01-17 16:34:42 -0300

    Update App.jsx

--- cliente/src/App.jsx
+++ cliente/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
      <h1 className="text-3xl font-bold underline">
-      Hello world! SOCIALLL
+      Hello world! Social App
     </h1> 
     </div>
   )
