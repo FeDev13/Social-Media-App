83996bd596d0cfbeef61508a1ee19ee485541a49
Juanpy <109622347+JuanpyMoscoloni@users.noreply.github.com>
2023-01-19 18:50:38 -0300

    agregando login a dev

---
+++ login/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Login from './Login';
+function App() {
+
+  return (
+    <div className='w-full flex justify-center bg-[#F3F5F7]' >
+        <Login/>
+    </div>
+  )
+}
+
+export default App
