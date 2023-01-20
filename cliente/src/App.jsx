7ae56a64561841b3b227426d7909e2e794e93af7
Juanpy <109622347+JuanpyMoscoloni@users.noreply.github.com>
2023-01-20 03:27:09 -0300

    profile agregado

--- cliente/src/App.jsx
+++ cliente/src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./Componentes/Home/MainHome"
 import Login from './Componentes/Login/Login';
 import Navbar from './Componentes/Navbar/Navbar'
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import UserProfile from './Componentes/Profile/UserProfile';
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
         <Routes>
         <Route path="/" element={[<Navbar/>,<Home/>]} />
         <Route path="/Login" element={<Login />} />
+        <Route path="/Profile" element={<UserProfile />} />
         </Routes>
       </BrowserRouter>
 
