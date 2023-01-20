c92aa670c90bdd01f25d1f9927aa472eb19da697
Juanpy <109622347+JuanpyMoscoloni@users.noreply.github.com>
2023-01-20 01:59:06 -0300

    Modal Centrado

--- login/src/Componentes/HomePageContent/AsideRight.jsx
+++ login/src/Componentes/HomePageContent/AsideRight.jsx
@@ -4,14 +4,14 @@ import "tw-elements";
 const AsideRight = () => {
   return (
     <>
-      <div className="flex  flex-col w-[15%] pt-[1.6%] gap-y-5 max-lg:w-[25%] max-sm:hidden">
-        <div className="fixed w-[15%] max-lg:w-[25%]  justify-center flex">
-          <div className="flex flex-col w-full rounded-lg p-3 shadow-lg bg-white">
+      <div className="flex  w-[15%] flex-col gap-y-5 pt-[1.6%] max-lg:w-[25%] max-sm:hidden">
+        <div className="fixed flex w-[15%]  justify-center max-lg:w-[25%]">
+          <div className="flex w-full flex-col rounded-lg bg-white p-3 shadow-lg">
             <div className="flex flex-col justify-center">
               <div className="relative flex justify-center">
                 <img
                   src="https://img.freepik.com/free-vector/hand-painted-watercolor-pastel-sky-background_23-2148902771.jpg?w=2000"
-                  className="rounded-lg h-[90%] relative"
+                  className="relative h-[90%] rounded-lg"
                   alt=""
                 />
                 <img
@@ -20,30 +20,30 @@ const AsideRight = () => {
                   alt=""
                 />
               </div>
-              <h1 className="text-center font-bold text-lg">Username</h1>
-              <h3 className="text-center font-extralight text-xs">@username</h3>
-              <div className="w-[100%] flex my-3">
-                <div className="flex flex-col w-[33%] items-center">
-                  <h1 className="font-bold text-lg">100</h1>
+              <h1 className="text-center text-lg font-bold">Username</h1>
+              <h3 className="text-center text-xs font-extralight">@username</h3>
+              <div className="my-3 flex w-[100%]">
+                <div className="flex w-[33%] flex-col items-center">
+                  <h1 className="text-lg font-bold">100</h1>
                   <h3 className="text-xs font-extralight opacity-60">Post</h3>
                 </div>
-                <div className="flex flex-col w-[33%] items-center">
-                  <h1 className="font-bold text-lg">100</h1>
+                <div className="flex w-[33%] flex-col items-center">
+                  <h1 className="text-lg font-bold">100</h1>
                   <h3 className="text-xs font-extralight opacity-60">
                     Followers
                   </h3>
                 </div>
-                <div className="flex flex-col w-[33%] items-center">
-                  <h1 className="font-bold text-lg">100</h1>
+                <div className="flex w-[33%] flex-col items-center">
+                  <h1 className="text-lg font-bold">100</h1>
                   <h3 className="text-xs font-extralight opacity-60">
                     Following
                   </h3>
                 </div>
               </div>
             </div>
-            <a className="w-[100%] relative flex items-center">
-              <div className="w-full flex items-center ">
-              {/* <svg
+            <a className="relative flex w-[100%] items-center">
+              <div className="flex w-full items-center ">
+                {/* <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
@@ -58,121 +58,123 @@ const AsideRight = () => {
                 />
               </svg> */}
 
-              <button
-                type="button"
-                class="w-full px-6
+                <button
+                  type="button"
+                  class="w-full rounded
+      bg-black
+      px-6
       py-2.5
-      bg-blue-600
-      text-white
-      font-medium
       text-xs
-      leading-tight
+      font-medium
       uppercase
-      rounded
+      leading-tight
+      text-white
       shadow-md
-      hover:bg-blue-700 hover:shadow-lg
-      focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0
-      active:bg-blue-800 active:shadow-lg
-      transition
-      duration-150
-      ease-in-out"
-                data-bs-toggle="modal"
-                data-bs-target="#exampleModal"
-              >
-                Create Post
-              </button>
+      transition duration-150
+      ease-in-out hover:bg-black hover:shadow-lg focus:bg-black
+      focus:shadow-lg focus:outline-none
+      focus:ring-0
+      active:bg-black
+      active:shadow-lg"
+                  data-bs-toggle="modal"
+                  data-bs-target="#exampleModal"
+                >
+                  Create Post
+                </button>
               </div>
+
               <div
-                class="modal fade fixed top-0 left-0 hidden w-full h-full outline-none overflow-x-hidden overflow-y-auto"
+                class="modal fade fixed top-0 left-0 hidden  h-full w-full  overflow-y-auto overflow-x-hidden outline-none"
                 id="exampleModal"
                 tabindex="-1"
                 aria-labelledby="exampleModalLabel"
                 aria-hidden="true"
               >
-                <div class="modal-dialog relative w-auto pointer-events-none">
-                  <div class="modal-content border-none shadow-lg relative flex flex-col w-full pointer-events-auto bg-white bg-clip-padding rounded-md outline-none text-current">
-                    <div class="modal-header flex flex-shrink-0 items-center justify-between p-4 border-b border-gray-200 rounded-t-md">
-                      <h5
-                        class="text-xl font-medium leading-normal text-gray-800"
-                        id="exampleModalLabel"
-                      >
-                        Create Post
-                      </h5>
-                      <button
-                        type="button"
-                        class="btn-close box-content w-4 h-4 p-1 text-black border-none rounded-none opacity-50 focus:shadow-none focus:outline-none focus:opacity-100 hover:text-black hover:opacity-75 hover:no-underline"
-                        data-bs-dismiss="modal"
-                        aria-label="Close"
-                      ></button>
-                    </div>
-                    <div class="modal-body relative p-4">
-                    <div className="relative p-[2%] gap-5 flex w-[100%] ">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-8 h-8 "
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
-            />
-          </svg>
-          <input
-            className="w-full text-sm pl-8 pr-16 p-2 rounded-lg outline-none bg-gray-100"
-            placeholder="What's going on?"
-          />
-
-        </div>
-                    </div>
-                    <div class="modal-footer flex flex-shrink-0 flex-wrap items-center justify-end p-4 border-t border-gray-200 rounded-b-md">
-                      <button
-                        type="button"
-                        class="px-6
-          py-2.5
+                <div className="flex h-full w-full items-center justify-center bg-slate-500 bg-opacity-40 ">
+                  <div class="modal-dialog pointer-events-none relative w-[50%]">
+                    <div class="modal-content pointer-events-auto relative flex w-full flex-col rounded-md border-none bg-white bg-clip-padding text-current shadow-lg outline-none">
+                      <div class="modal-header flex flex-shrink-0 items-center justify-between rounded-t-md border-b border-gray-200 p-4">
+                        <h5
+                          class="text-xl font-medium leading-normal text-gray-800"
+                          id="exampleModalLabel"
+                        >
+                          Create Post
+                        </h5>
+                        <button
+                          type="button"
+                          class="btn-close box-content h-4 w-4 rounded-none border-none p-1 text-black opacity-50 hover:text-black hover:no-underline hover:opacity-75 focus:opacity-100 focus:shadow-none focus:outline-none"
+                          data-bs-dismiss="modal"
+                          aria-label="Close"
+                        ></button>
+                      </div>
+                      <div class="modal-body relative p-4">
+                        <div className="relative flex w-[100%] gap-5 p-[2%] ">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth={1.5}
+                            stroke="currentColor"
+                            className="h-8 w-8 "
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
+                            />
+                          </svg>
+                          <input
+                            className="w-full rounded-lg bg-gray-100 p-2 pl-8 pr-16 text-sm outline-none"
+                            placeholder="What's going on?"
+                          />
+                        </div>
+                      </div>
+                      <div class="modal-footer flex flex-shrink-0 flex-wrap items-center justify-end rounded-b-md border-t border-gray-200 p-4">
+                        <button
+                          type="button"
+                          class="rounded
           bg-purple-600
-          text-white
-          font-medium
+          px-6
+          py-2.5
           text-xs
-          leading-tight
+          font-medium
           uppercase
-          rounded
+          leading-tight
+          text-white
           shadow-md
-          hover:bg-purple-700 hover:shadow-lg
-          focus:bg-purple-700 focus:shadow-lg focus:outline-none focus:ring-0
-          active:bg-purple-800 active:shadow-lg
-          transition
-          duration-150
-          ease-in-out"
-                        data-bs-dismiss="modal"
-                      >
-                        Close
-                      </button>
-                      <button
-                        type="button"
-                        class="px-6
-      py-2.5
-      bg-blue-600
-      text-white
-      font-medium
-      text-xs
-      leading-tight
-      uppercase
+          transition duration-150
+          ease-in-out hover:bg-purple-700 hover:shadow-lg focus:bg-purple-700
+          focus:shadow-lg focus:outline-none
+          focus:ring-0
+          active:bg-purple-800
+          active:shadow-lg"
+                          data-bs-dismiss="modal"
+                        >
+                          Close
+                        </button>
+                        <button
+                          type="button"
+                          class="ml-1
       rounded
-      shadow-md
-      hover:bg-blue-700 hover:shadow-lg
-      focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0
-      active:bg-blue-800 active:shadow-lg
-      transition
-      duration-150
-      ease-in-out
-      ml-1"
-                      >
-                        Publish
-                      </button>
+      bg-blue-600
+      px-6
+      py-2.5
+      text-xs
+      font-medium
+      uppercase
+      leading-tight
+      text-white
+      shadow-md transition
+      duration-150 ease-in-out hover:bg-blue-700 hover:shadow-lg
+      focus:bg-blue-700 focus:shadow-lg
+      focus:outline-none
+      focus:ring-0
+      active:bg-blue-800
+      active:shadow-lg"
+                        >
+                          Publish
+                        </button>
+                      </div>
                     </div>
                   </div>
                 </div>
--- login/src/Componentes/HomePageContent/HomeCenter.jsx
+++ login/src/Componentes/HomePageContent/HomeCenter.jsx
@@ -1,11 +1,7 @@
 import React from "react";
 import { useState, useEffect } from "react";
 const HomeCenter = () => {
-  const [count, setCount] = useState(0);
 
-  function handleSubmit() {
-    setCount(count + 1);
-  }
 
   return (
     <div className="w-[65%] py-[2%] px-[2%] max-ms:px-0 flex-col gap-y-8 max-md:pl-0 max-lg:w-[70%] flex max-sm:w-[95%] h-[50%]">
@@ -58,9 +54,9 @@ const HomeCenter = () => {
               fill="none"
               viewBox="0 0 24 24"
               strokeWidth={1.5}
-              stroke="currentColor"
+              stroke="red"
               className="w-6 h-6"
-              onClick={handleSubmit}
+             
             >
               <path
                 strokeLinecap="round"
@@ -68,7 +64,7 @@ const HomeCenter = () => {
                 d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
               />
             </svg>
-            <h4 className="text-xs font-extralight">{count}</h4>
+            <h4 className="text-xs font-extralight">120</h4>
           </div>
           <div className="flex gap-2 items-center">
             <svg
@@ -172,9 +168,9 @@ const HomeCenter = () => {
               fill="none"
               viewBox="0 0 24 24"
               strokeWidth={1.5}
-              stroke="currentColor"
+              stroke="red"
               className="w-6 h-6"
-              onClick={handleSubmit}
+             
             >
               <path
                 strokeLinecap="round"
@@ -182,7 +178,7 @@ const HomeCenter = () => {
                 d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
               />
             </svg>
-            <h4 className="text-xs font-extralight">{count}</h4>
+            <h4 className="text-xs font-extralight">400</h4>
           </div>
           <div className="flex gap-2 items-center">
             <svg
@@ -199,7 +195,7 @@ const HomeCenter = () => {
                 d="M8.625 9.75a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H8.25m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H12m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0h-.375m-13.5 3.01c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.184-4.183a1.14 1.14 0 01.778-.332 48.294 48.294 0 005.83-.498c1.585-.233 2.708-1.626 2.708-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z"
               />
             </svg>
-            <h4 className="text-xs font-extralight">12 Comments</h4>
+            <h4 className="text-xs font-extralight">24 Comments</h4>
           </div>
         </div>
         <div className="relative p-[2%] gap-5 flex w-[100%] ">
