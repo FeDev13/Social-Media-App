42b7a4f63fa6e57f5901bf93e03e4d91c1aee526
Anderson Ponce <nerudasho@gmail.com>
2023-01-22 15:03:21 -0300

    Footer

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './estilos/style.css';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="App">
+    
+
+<Footer/>
+
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const Footer = () => {
+  return (
+    <div className='foo'>
+      
+
+   <p className='copy'>©2023 Grupo-3 Bienvenido</p>
+ 
+
+    </div>
+  )
+}
+
+export default Footer
