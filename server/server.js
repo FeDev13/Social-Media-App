39da1d0a21238da933311f8e0929705f3d1def04
Juanpy <109622347+JuanpyMoscoloni@users.noreply.github.com>
2023-01-24 02:12:02 -0300

    Messages

--- server/server.js
+++ server/server.js
@@ -1,15 +1,12 @@
-require("dotenv").config();
 const express = require("express");
-const port =  process.env.PORT || 3000;
+const mongose = require("mongoose");
 const puertoMongo = process.env.LOCALHOST;
 const app = express();
-var cors = require("cors");
-
-const routerU = require("./routes/users.route");
-const routerPosts = require("./routes/posts.route");
-
+const socket = require("socket.io");
+const cors = require("cors");
+require("dotenv").config();
 /**Cadena conexion con mongo */
-const mongose = require("mongoose");
+
 mongose
   .connect(puertoMongo)
   .then(() => console.log("Connect MongoDB"))
@@ -17,13 +14,48 @@ mongose
     console.error(err);
   });
 /**Fin de cadena conexion */
+const routerU = require("./routes/users.route");
+const routerPosts = require("./routes/posts.route");
+const routerMessages = require("./routes/messages");
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(routerU);
 app.use(routerPosts);
+app.use(routerMessages);
 app.use(express.static(__dirname));
 
-app.listen(port, () => {
-  console.log(`Servidor corriendo por el puerto` + " " + port);
+
+
+
+
+app.listen(process.env.PORT, () => {
+  console.log(`Servidor corriendo por el puerto` + " " + process.env.PORT);
+});
+
+const server = app.listen(process.env.PORT, () =>
+  console.log(`Server started on ${process.env.PORT}`)
+);
+
+const io = socket(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    credentials: true,
+  },
+});
+
+global.onlineUsers = new Map();
+io.on("connection", (socket) => {
+  global.chatSocket = socket;
+  socket.on("add-user", (userId) => {
+    onlineUsers.set(userId, socket.id);
+  });
+
+  socket.on("send-msg", (data) => {
+    const sendUserSocket = onlineUsers.get(data.to);
+    if (sendUserSocket) {
+      socket.to(sendUserSocket).emit("msg-recieve", data.msg);
+    }
+  });
 });
