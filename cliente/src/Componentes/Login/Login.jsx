2cebd5d1733cea9dc143b93cdb4d21f4a0d8c57e
Alejandro Agra <aleagra08@hotmail.com>
2023-01-23 21:45:06 -0300

    profile

--- cliente/src/Componentes/Login/Login.jsx
+++ cliente/src/Componentes/Login/Login.jsx
@@ -7,22 +7,36 @@ const Login = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [user, setUser] = useState(null);
+
+  const [text, setText] = useState("created");
+  const [activeBtn, setActiveBtn] = useState("Mensaje");
+  const bgImage =
+    "https://images.unsplash.com/photo-1673447043169-a309c86f822c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDczfGlVSXNuVnRqQjBZfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60";
+  const userimage =
+    "https://images.unsplash.com/photo-1670582725604-ee64ac849e4f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDUwfGhtZW52UWhVbXhNfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60";
+
+  const activeBtnStyles =
+    "bg-blue-600 text-white font-bold p-2 rounded-lg shadow-lg w-40 outline-none";
+
+  const notActiveBtnStyles =
+    "bg-primary text-black mr-4 font-bold p-2 rounded-lg w-40 outline-none";
+
   const handleSubmit = async (e) => {
     e.preventDefault();
+
     try {
-      var bodyFormData = {
+      const user = await axios.post(url, {
         username: username,
         password: password,
-      };
-
-      const resp = await axios.post(url, bodyFormData);
+      });
       window.localStorage.setItem("loggedNoteappUser", JSON.stringify(user));
       setUser(user.data.user);
       setUsername("");
       setPassword("");
-      console.log(resp.data);
-    } catch (error) {
-      console.log(error.response);
+
+      console.log(user);
+    } catch (exception) {
+      console.log("Error al loguearse");
     }
   };
 
@@ -30,15 +44,16 @@ useEffect(() => {
     const loggedUserJSON = window.localStorage.getItem("loggedNoteappUser");
     if (loggedUserJSON) {
       const user = JSON.parse(loggedUserJSON);
-      setUser(user.data.user);
+      setUser(user);
     }
   }, []);
   const hadndleLogout = () => {
     setUsername(null);
     window.localStorage.removeItem("loggedNoteappUser");
+    window.localStorage.removeItem("theme");
+    window.localStorage.removeItem("color");
   };
-  console.log(username);
-  console.log(user);
+
     const loginForm = () => (
       <div className="flex w-full justify-center bg-[#F3F5F7] dark:bg-black h-screen">
         <div className="flex w-[40%] max-lg:w-[70%] max-md:w-[80%] max-sm:w-full max-sm:p-0 items-center justify-center p-[2%]">
@@ -108,7 +123,69 @@ useEffect(() => {
         </div>
       </div>)
 
+const logoutForm = () => (   <div className="relative pb-2 h-full justify-center items-center">
+<div className="flex flex-col pb-5 dark:text-white">
+  <div className="relative flex flex-col mb-7">
+    {/* imagen de fondo */}
+    <div className="flex flex-col justify-center items-center">
+      <img
+        src={bgImage} //hacerlo dinamico con la db
+        alt=""
+        className="w-full h-60 2xl:h-510 shadow-lg object-cover rounded-lg"
+      />
+      <img
+        src={userimage}
+        className="rounded-full w-40 h-40 -mt-10 shadow-2xl object-cover"
+        alt=""
+      />
+      <h1 className="font-bold text-3xl text-center mt-3 mb-10">
+        Usuario anonimo
+      </h1>{" "}
+      <h5 className=" text-center mb-8 mt-0">
+        {" "}
+        Mar del Plata, Argentina
+      </h5>
+      <div className=" flex flex-row mt-0 mb-10">
+        @user
+        <li className=" mx-8">Trabajo actual</li>
+        <li>Algo mas</li>
+      </div>
+      {/*hacerlo dinamico*/}
+    </div>
 
+    {/* botones */}
+    <div className="text-center mb-7">
+      <button
+        type="button"
+        onClick={(e) => {
+
+          setText(e.target.textContent);
+          setActiveBtn("Mensaje");
+        }}
+        className={` dark:text-white ${
+          activeBtn === "Mensaje" ? activeBtnStyles : notActiveBtnStyles
+        }`} 
+      >
+    Mensaje
+      </button>
+      <button
+        type="button"
+        onClick={(e) => {
+          setText(e.target.textContent);
+          setActiveBtn("share");
+        }}
+        className={` dark:text-white ${
+          activeBtn === "share" ? activeBtnStyles : notActiveBtnStyles
+        }`}
+      >
+        Compartir perfil
+      </button>
+      <a href="/Login">
+      <button  onClick={hadndleLogout} className="dark:text-white bg-blue-600 text-white font-bold p-2 rounded-lg shadow-lg w-40 outline-none">salir</button></a>
+    </div>
+  </div>
+</div>
+</div>)
 
   return (
     <>
