27f5be5cbef97c6a9b0eb34777242a91adbbf3e0
Federico Robledo <federicorobledo85@gmail.com>
2023-03-30 21:38:38 -0300

    final merge

--- cliente/src/App.jsx
+++ cliente/src/App.jsx
@@ -1,14 +1,42 @@
-import React from 'react';
-
-function App() {
-
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./components/Home/MainHome";
+import SetAvatar from "./components/SetAvatar";
+import Chat from "./pages/Chat";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import { Search } from "./components/Navbar/Search";
+import { ThemeProvider } from "./components/Navbar/ThemeContext";
+import ProfileUsers from "./components/Profile/UsersProfile";
+import Profile from "./components/Profile/MyProfile";
+export default function App() {
   return (
-    <div className="App">
-     <h1 className="text-3xl font-bold underline">
-      Hello world! SOCIALLL
-    </h1> 
-    </div>
-  )
+    <>
+      <div>
+        <ThemeProvider>
+          <BrowserRouter>
+            <Routes>
+              <Route
+                path="/"
+                element={[
+                  <div>
+                    {" "}
+                    <Navbar />,<Home />
+                  </div>,
+                ]}
+              />
+              <Route path="/register" element={<Register />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/setAvatar" element={<SetAvatar />} />
+              <Route path="/Search/:buscar" element={<Search />} />
+              <Route path="/chat" element={[<Navbar />, <Chat />]} />
+              <Route path="/Profile/:id" element={[<ProfileUsers />]} />
+              <Route path="/Profile" element={[<Profile/>]} />
+            </Routes>
+          </BrowserRouter>
+        </ThemeProvider>
+      </div>
+    </>
+  );
 }
-
-export default App
